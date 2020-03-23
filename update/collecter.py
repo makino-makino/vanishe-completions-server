eae2cd5e9926a94cbb75270e46fb0548e5408185
akakou <akakou571@gmail.com>
2020-03-24 02:30:41 +0900

    ツイートをDBに追加した

--- update/collecter.py
+++ update/collecter.py
@@ -4,23 +4,27 @@ from pprint import pprint
 import tweepy
 from config import consumer_key, consumer_secret, access_token, access_secret
 
+from models import find_or_add_tweet, session, Tweet
+
 ACCOUNT = 'a_a_vanilove'
+COUNT = 20
 
 def main():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
 
-    data = []
+    results = api.user_timeline(screen_name=ACCOUNT, count=COUNT)
+    for r in results:
+        tweet = Tweet()
+        tweet.twitterId = r.id
+        tweet.text = r.text
 
-    tweets = api.user_timeline(screen_name=ACCOUNT, count=10)
-    for tweet in tweets:
-        data.append({'id': tweet.id, 'text': tweet.text})
+        find_or_add_tweet(session, tweet)
+        
+    
+    session.commit()
 
-    pprint(data)
-
-    # with open('data.json', 'w') as f:
-        # json.dump(data, f, ensure_ascii=False)
 
 
 if __name__ == '__main__':
