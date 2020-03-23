11fc599f37f70b842de371fd190ba465eda6e713
akakou <akakou571@gmail.com>
2020-03-24 02:18:11 +0900

    モデルを追加

---
+++ update/models.py
@@ -0,0 +1,48 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, Text, Float, DateTime
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+ENGINE = create_engine('sqlite:///db.sqlite3', echo=True)
+Base = declarative_base()
+
+session = scoped_session(
+    sessionmaker(
+        bind = ENGINE,
+        autocommit=False,
+    )
+)
+
+class Tweet(Base):
+    '''ツイートのデータ'''
+    __tablename__ = 'tweets'
+
+    _id = Column('id', Integer, primary_key = True)
+    twitterId = Column('twitterId', Integer)
+    text = Column('text', Text)
+
+class Word(Base):
+    '''辞書ワードのデータ'''
+    __tablename__ = 'words'
+
+    _id = Column('id', Integer, primary_key = True)
+    text = Column('text', Text)
+    yomi = Column('yomi', Text)
+
+
+Base.metadata.create_all(ENGINE)
+
+
+if __name__=='__main__':
+    tweet = Tweet()
+    tweet.twitterId = 1
+    tweet.text = 'hello'
+
+    word = Word()
+    word.yomi = 'ばにしぇだよ'
+    word.text = 'ばにしぇだよ〜〜〜www'
+
+    session.add(tweet)
+    session.add(word)
+
+    session.commit()
