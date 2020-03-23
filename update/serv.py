581d31e48bb6de8b8342fbbd9939161aee7b8ae5
akakou <akakou571@gmail.com>
2020-03-24 02:45:10 +0900

    DB内のワードを送信するようにした

--- update/serv.py
+++ update/serv.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
+import urllib.parse
+
+
+from models import session, Word
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -8,9 +13,21 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/update", methods=['GET'])
 @cross_origin()
 def update():
-    first = request.args.get('fitst')
-    result = 'hello'
-    return jsonify({"result": result})
+    first = request.args.get('first', default=0, type=int)
+
+    words = session.query(Word).\
+        filter(Word._id >= first).\
+        all()
+    
+    result = []
+    for word in words:
+        result.append({
+            'id': urllib.parse.quote(str(word._id)),
+            'yomi': urllib.parse.quote(word.yomi),
+            'kaki': urllib.parse.quote(word.kaki)
+        })
+    
+    return jsonify(result)
 
 
 
